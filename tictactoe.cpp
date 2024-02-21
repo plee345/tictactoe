d3f127d2c4c2beb459c88282bc59c3e834cece00
plee345 <plee09902@gmail.com>
2024-02-20 21:20:48 -0500

    Adding sample code

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
 
+std::vector<int> checkPosition();
+std::string winCondition();
+
+
+int main(){
+    bool win = false;
+    std::vector<std::vector<int>> board;
+    std::cout << "Let's play tic-tac-toe!" << std::endl;
+    while (!win){
+        std::cout << "Please make a valid move." << std::endl;
+        bool valid = false;
+        while (!valid){
+            
+        }
+    }
+    return 0;
+}
+
+std::vector<int> checkPosition(std::vector<int> &board){
+
+}
+
+std::string winCondition(std::string &move){
+
+}
