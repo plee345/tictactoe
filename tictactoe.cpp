d87592a851ca2e2864d98300a6ce6a47108f94e8
plee345 <plee09902@gmail.com>
2024-02-20 21:41:58 -0500

    Add necessary lauch json files

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
-std::vector<int> checkPosition();
-std::string winCondition();
+std::string checkPosition(std::vector<std::vector<std::string>>& board);
+std::string winCondition(std::string& move);
 
 
 int main(){
     bool win = false;
-    std::vector<std::vector<int>> board;
+    std::vector<std::vector<int>>{
+        "000", "000", "000"
+    };
     std::cout << "Let's play tic-tac-toe!" << std::endl;
     while (!win){
         std::cout << "Please make a valid move." << std::endl;
@@ -18,10 +22,10 @@ int main(){
     return 0;
 }
 
-std::vector<int> checkPosition(std::vector<int> &board){
-
+std::string checkPosition(std::vector<std::vector<std::string>>& board){
+    
 }
 
-std::string winCondition(std::string &move){
-
+std::string winCondition(std::string& move){
+    
 }
